78699f3cec12802cc0397b23b0158a0dbe30cd44
nheingit <NHeinDev@Gmail.com>
2023-05-10 17:15:41 -0500

    changed tsconfig

--- index.ts
+++ index.ts
@@ -2,126 +2,127 @@ import {
   ConnectorNotFoundError,
   WindowProvider,
   InjectedConnector,
-  InjectedConnectorOptions
-} from '@wagmi/core'
+  InjectedConnectorOptions,
+} from "@wagmi/core";
 import {
   ProviderRpcError,
   ResourceNotFoundRpcError,
   UserRejectedRequestError,
   getAddress,
-} from 'viem'
-import type { Address } from 'abitype'
-import type { Chain } from '@wagmi/core/chains'
+} from "viem";
+import type { Address } from "abitype";
+import type { Chain } from "@wagmi/core/chains";
 
 declare global {
   interface Window {
     phantom: {
-      ethereum: WindowProvider
-    }
+      ethereum: WindowProvider;
+    };
   }
 }
 
 export class PhantomConnector extends InjectedConnector {
-  readonly id = 'phantom'
+  readonly id = "phantom";
 
-  protected shimDisconnectKey = `${this.id}.shimDisconnect`
-
+  protected shimDisconnectKey = `${this.id}.shimDisconnect`;
 
   constructor({
     chains,
     options: options_,
   }: {
-    chains?: Chain[]
-    options?: InjectedConnectorOptions
+    chains?: Chain[];
+    options?: InjectedConnectorOptions;
   } = {}) {
     const options = {
-      name: 'Phantom',
+      name: "Phantom",
       shimDisconnect: true,
       getProvider() {
         function getReady(ethereum?: WindowProvider) {
-          const isPhantom = !!ethereum?.isPhantom
-          if (!isPhantom) return
-          return ethereum
+          const isPhantom = !!ethereum?.isPhantom;
+          if (!isPhantom) return;
+          return ethereum;
         }
 
-        if (typeof window === 'undefined') return
-        const ethereum = window?.phantom?.ethereum as WindowProvider | undefined
-        if (ethereum?.providers) return ethereum.providers.find(getReady)
-        return getReady(ethereum)
+        if (typeof window === "undefined") return;
+        const ethereum = window?.phantom?.ethereum as
+          | WindowProvider
+          | undefined;
+        if (ethereum?.providers) return ethereum.providers.find(getReady);
+        return getReady(ethereum);
       },
       ...options_,
-    }
-    super({ chains, options })
+    };
+    super({ chains, options });
   }
 
   async connect({ chainId }: { chainId?: number } = {}) {
     try {
-      const provider = await this.getProvider()
-      if (!provider) throw new ConnectorNotFoundError()
+      const provider = await this.getProvider();
+      if (!provider) throw new ConnectorNotFoundError();
 
       if (provider.on) {
-        provider.on('accountsChanged', this.onAccountsChanged)
-        provider.on('chainChanged', this.onChainChanged)
-        provider.on('disconnect', this.onDisconnect)
+        provider.on("accountsChanged", this.onAccountsChanged);
+        provider.on("chainChanged", this.onChainChanged);
+        provider.on("disconnect", this.onDisconnect);
       }
 
-      this.emit('message', { type: 'connecting' })
+      this.emit("message", { type: "connecting" });
 
       // Attempt to show wallet select prompt with `wallet_requestPermissions` when
       // `shimDisconnect` is active and account is in disconnected state (flag in storage)
-      let account: Address | null = null
+      let account: Address | null = null;
       if (
         this.options?.shimDisconnect &&
         !this.storage?.getItem(this.shimDisconnectKey)
       ) {
-        account = await this.getAccount().catch(() => null)
-        const isConnected = !!account
+        account = await this.getAccount().catch(() => null);
+        const isConnected = !!account;
         if (isConnected)
           // Attempt to show another prompt for selecting wallet if already connected
           try {
             await provider.request({
-              method: 'wallet_requestPermissions',
+              method: "wallet_requestPermissions",
               params: [{ eth_accounts: {} }],
-            })
+            });
             // User may have selected a different account so we will need to revalidate here.
-            account = await this.getAccount()
+            account = await this.getAccount();
           } catch (error) {
             if (this.isUserRejectedRequestError(error))
-              throw new UserRejectedRequestError(error as Error)
+              throw new UserRejectedRequestError(error as Error);
             if (
               (error as ProviderRpcError).code ===
               new ResourceNotFoundRpcError(error as ProviderRpcError).code
             )
-              throw error
+              throw error;
           }
       }
 
       if (!account) {
         const accounts = await provider.request({
-          method: 'eth_requestAccounts',
-        })
-        account = getAddress(accounts[0] as string)
+          method: "eth_requestAccounts",
+        });
+        account = getAddress(accounts[0] as string);
       }
 
       // Switch to chain if provided
-      let id = await this.getChainId()
-      let unsupported = this.isChainUnsupported(id)
+      let id = await this.getChainId();
+      let unsupported = this.isChainUnsupported(id);
       if (chainId && id !== chainId) {
-        const chain = await this.switchChain(chainId)
-        id = chain.id
-        unsupported = this.isChainUnsupported(id)
+        const chain = await this.switchChain(chainId);
+        id = chain.id;
+        unsupported = this.isChainUnsupported(id);
       }
 
       if (this.options?.shimDisconnect)
-        this.storage?.setItem(this.shimDisconnectKey, true)
+        this.storage?.setItem(this.shimDisconnectKey, true);
 
-      return { account, chain: { id, unsupported }, provider }
+      return { account, chain: { id, unsupported }, provider };
     } catch (error) {
       if (this.isUserRejectedRequestError(error))
-        throw new UserRejectedRequestError(error as Error)
+        throw new UserRejectedRequestError(error as Error);
       if ((error as ProviderRpcError).code === -32002)
-        throw new ResourceNotFoundRpcError(error as ProviderRpcError)
-      throw error
+        throw new ResourceNotFoundRpcError(error as ProviderRpcError);
+      throw error;
     }
   }
 }
